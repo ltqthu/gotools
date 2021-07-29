cc2ad0ef34cc4989295613904f17ba536a226e67
liutq <1355583269@qq.com>
2021-07-29 17:35:10 +0800

    update

--- cmd/vscode/main.go
+++ cmd/vscode/main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"github.com/ltqthu/gotools/pkg/vscode"
 	"log"
 )
 
+//go run cmd/vscode/main.go
+//go build cmd/vscode/main.go
 func main() {
-	fmt.Println("hello world")
-	log.Println("hello world")
+	code := vscode.NewVscodeService()
+
+	binPath, err := code.MingwSrv.GetMingwBin()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(binPath)
+
+	includePath, err := code.MingwSrv.GetMingwInclude()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(includePath)
 }
